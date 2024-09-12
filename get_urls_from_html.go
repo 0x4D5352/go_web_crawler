afce934b4fa0690cfd6fe0f0d0c393581e456223
Joshua Nussbaum <67082011+joshuanussbaum@users.noreply.github.com>
2024-09-12 16:40:14 -0500

    implement geturls function

--- get_urls_from_html.go
+++ get_urls_from_html.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"errors"
+	"golang.org/x/net/html"
+	"log"
+	"net/url"
+	"strings"
+)
+
 func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
-	return []string{}, nil
+	if len(htmlBody) == 0 {
+		return []string{}, errors.New("Error: HTML missing body")
+	}
+	htmlReader := strings.NewReader(htmlBody)
+	htmlNode, err := html.Parse(htmlReader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	urls := []string{}
+	var traverseTree func(*html.Node)
+	traverseTree = func(node *html.Node) {
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, attribute := range node.Attr {
+				if attribute.Key == "href" {
+					newURL, err := url.Parse(attribute.Val)
+					if err != nil {
+						log.Fatal(err)
+					}
+					var fullURL string
+					if newURL.Host != "" {
+						fullURL = newURL.Scheme + "://" + newURL.Host + newURL.Path
+					} else {
+						fullURL = rawBaseURL
+						if strings.Split(newURL.Path, "")[0] != "/" {
+							fullURL += "/"
+						}
+						fullURL += newURL.Path
+					}
+					urls = append(urls, fullURL)
+					break
+				}
+			}
+		}
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			traverseTree(child)
+		}
+	}
+	traverseTree(htmlNode)
+
+	return urls, nil
 }
