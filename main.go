35811a373c5f19f80e4609b89cdc408587a119c1
Joshua Nussbaum <67082011+0x4D5352@users.noreply.github.com>
2024-09-12 18:15:53 -0500

    add get_html function

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -9,13 +13,42 @@ func main() {
 	args := os.Args[1:]
 
 	if len(args) < 1 {
-		fmt.Println("no website provided")
-		os.Exit(1)
+		log.Fatal("no website provided")
 	}
 	if len(args) > 1 {
-		fmt.Println("too many arguments provided")
-		os.Exit(1)
+		log.Fatal("too many arguments provided")
 	}
 
-	fmt.Printf("starting crawl of: %s", args[0])
+	rawURL := args[0]
+	fmt.Printf("starting crawl of: %s", rawURL)
+	body, err := getHTML(rawURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(body)
+}
+
+func getHTML(rawURL string) (string, error) {
+	webpage, err := http.Get(rawURL)
+	if err != nil {
+		return "", fmt.Errorf("error - failed to fetch webpage for %s: %w", rawURL, err)
+	}
+	defer webpage.Body.Close()
+	if webpage.StatusCode > 399 {
+		return "", fmt.Errorf("error - website returned status code: %w", webpage.Status)
+	}
+	if len(webpage.Header["Content-Type"]) == 0 || len(webpage.Header["Content-Type"]) > 1 {
+		return "", fmt.Errorf("error - incorrect number of content types: %d", len(webpage.Header["Content-Type"]))
+	}
+	if webpage.Header["Content-Type"][0] != "text/html" {
+		return "", fmt.Errorf("error - incorrect content-type: %w", webpage.Header["Content-Type"])
+	}
+	body, err := io.ReadAll(webpage.Body)
+	if err != nil {
+		return "", fmt.Errorf("error - failed to read webpage body: %w", err)
+	}
+	if len(body) == 0 {
+		return "", errors.New("error - webpage body was empty")
+	}
+	return string(body), nil
 }
