09d9bf60c38de55cea4530ec90b3e9f213545873
Joshua Nussbaum <67082011+0x4D5352@users.noreply.github.com>
2024-09-12 17:38:56 -0500

    update get_url func based on solution

--- get_urls_from_html.go
+++ get_urls_from_html.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"golang.org/x/net/html"
-	"log"
 	"net/url"
 	"strings"
 )
 
 func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse base URL: %v", err)
+	}
+
 	if len(htmlBody) == 0 {
 		return []string{}, errors.New("Error: HTML missing body")
 	}
 	htmlReader := strings.NewReader(htmlBody)
 	htmlNode, err := html.Parse(htmlReader)
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("Error parsing HTML: %w", err)
 	}
 	var urls []string
 	var traverseTree func(*html.Node)
@@ -23,21 +28,14 @@ func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
 		if node.Type == html.ElementNode && node.Data == "a" {
 			for _, attribute := range node.Attr {
 				if attribute.Key == "href" {
-					newURL, err := url.Parse(attribute.Val)
+					href, err := url.Parse(attribute.Val)
 					if err != nil {
-						log.Fatal(err)
+						fmt.Printf("couldn't parse href '%v': %v\n", attribute.Val, err)
+						continue
 					}
-					var fullURL string
-					if newURL.Host != "" {
-						fullURL = newURL.Scheme + "://" + newURL.Host + newURL.Path
-					} else {
-						fullURL = rawBaseURL
-						if strings.Split(newURL.Path, "")[0] != "/" {
-							fullURL += "/"
-						}
-						fullURL += newURL.Path
-					}
-					urls = append(urls, fullURL)
+
+					resolvedURL := baseURL.ResolveReference(href)
+					urls = append(urls, resolvedURL.String())
 					break
 				}
 			}
