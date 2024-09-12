1d7ad4f3b57578af4ddb10bc5530865a509f09e8
Joshua Nussbaum <67082011+joshuanussbaum@users.noreply.github.com>
2024-09-12 16:40:26 -0500

    add basic CLI functionality

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(args) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	fmt.Printf("starting crawl of: %s", args[0])
 }
