0cd086dec9bcc1271c3e39082a54344ca304bcb9
Joshua Nussbaum <67082011+joshuanussbaum@users.noreply.github.com>
2024-10-10 12:26:17 -0500

    implement robots.txt parsing

--- check_robots_txt.go
+++ check_robots_txt.go
@@ -5,11 +5,11 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"net/url"
+	"os"
+	"strings"
 )
 
 type RobotsTxt struct {
-	source     *url.URL
 	allowed    []string
 	disallowed []string
 }
@@ -35,18 +35,56 @@ func (cfg *config) checkRobotsTxt(rawURL string) {
 		return
 	}
 	fmt.Println("robots.txt exists, checking their requests...")
-	parseRobotsTxt(&contents)
-	source, err := url.Parse(rawURL)
+	allowed, disallowed, err := parseRobotsTxt(contents)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// TODO: fill out the allowed and disallowed lists kind of like the getlinksfromURL function
 	// resolvedURL := cfg.baseURL.ResolveReference(href)
 	cfg.robots = RobotsTxt{
-		source: source,
+		allowed:    allowed,
+		disallowed: disallowed,
 	}
+	fmt.Printf("robots.txt:\n%+v\n", cfg.robots)
+	os.Exit(1)
 }
 
-func parseRobotsTxt(contents *[]byte) {
-	panic("unimplemented")
+func parseRobotsTxt(contents []byte) ([]string, []string, error) {
+	body := string(contents)
+	lines := strings.Split(body, "\n")
+	var allowed []string
+	var disallowed []string
+	var isOurUserAgent bool
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+		if line == "User-agent: *" {
+			isOurUserAgent = true
+			continue
+		} else if strings.Contains(line, "User-agent:") {
+			isOurUserAgent = false
+			continue
+		}
+		// else we're in an allow or disallow line
+		if !isOurUserAgent {
+			continue
+		}
+		group, path, exists := strings.Cut(line, ": ")
+		if !exists {
+			continue
+		}
+		switch group {
+		case "Allow":
+			allowed = append(allowed, path)
+		case "Disallow":
+			disallowed = append(disallowed, path)
+		default:
+			return nil, nil, fmt.Errorf("error: unexpected key/value pair %s", line)
+		}
+	}
+	if allowed == nil && disallowed == nil {
+		return nil, nil, fmt.Errorf("error: no valid groups in %s", lines)
+	}
+	return allowed, disallowed, nil
 }
